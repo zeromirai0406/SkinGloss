63dc3b23a27b1234d94008c63ec38a85e7e04999
zeromirai0406 <163638183+zeromirai0406@users.noreply.github.com>
2025-11-20 21:54:11 +0800

    Update firebase_config.py

--- config/firebase_config.py
+++ config/firebase_config.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore, storage
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate("skingloss-1d5bc-firebase-adminsdk-fbsvc-16f212df2a.json")
+    cred = credentials.Certificate("skingloss-1d5bc-firebase-adminsdk-fbsvc-9e980c47ae")
     firebase_admin.initialize_app(cred, {
         "storageBucket": "skingloss-1d5bc.appspot.com"  
     })
@@ -12,3 +12,4 @@ db = firestore.client()
 
 # Storage client
 bucket = storage.bucket()
+
